40098e1d970e3573a39dc2262d65c74c6cc26049
Hector Bernstorff <95435499+HectorBernstorff@users.noreply.github.com>
2023-09-17 12:14:58 -0400

    Added slide menu functionality

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-const innermenu = document.querySelector(".innerMenu");
 
-const dragging = (e) => {
-    innermenu.scrollLeft = e.pageX;
-}
+const rightArrow = document.querySelector(".page1 .Menu .rightArrowIcon");
+const leftArrow = document.querySelector(".page1 .Menu .leftArrowIcon");
 
-innermenu.addEventListener("mousemove", dragging);
+const rightArrowMobile = document.querySelector(".page1 .Menu .rightArrowIconMobile");
+const leftArrowMobile = document.querySelector(".page1 .Menu .leftArrowIconMobile");
+
+const menuList = document.querySelector(".innerMenu");
+
+const nav = document.querySelector(".navBar");
+let lastScrollY = window.scrollY;
+
+
+rightArrow.addEventListener("click", () => {
+    menuList.scrollLeft += 208;
+
+});
+
+leftArrow.addEventListener("click", () => {
+    menuList.scrollLeft -= 208;
+
+});
+
+rightArrowMobile.addEventListener("click", () => {
+    menuList.scrollLeft += 160;
+
+});
+
+leftArrowMobile.addEventListener("click", () => {
+    menuList.scrollLeft -= 160;
+
+});
+
+// window.addEventListener("scroll", () =>{
+    
+
+//     if (lastScrollY==0){
+//         console.log("ta em cima");
+//     }
+
+//     // console.log(lastScrollY);
+//     if (lastScrollY < window.scrollY) {
+//         nav.classList.add("navHidden");
+//     } else{
+//         nav.classList.remove("navHidden");
+//     }
+
+//     lastScrollY = window.scrollY;
+// });
+
+
+
 
